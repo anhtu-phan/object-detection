e129902662c17b4371f11bfd262d20c3cab717b1
anhtu95 <anhtu23795@gmail.com>
2022-11-16 17:40:25 +0100

    try to fix plot image

--- run_demo_server.py
+++ run_demo_server.py
@@ -4,15 +4,15 @@ import argparse
 import torch
 import matplotlib.pyplot as plt
 import torchvision.transforms as T
-from PIL import Image
+from PIL import Image, ImageDraw
 
 from detr.models import build_model as build_model_detr
 
 app = Flask(__name__)
 
 CLASSES = ['almond', 'apple', "mango", "N/A", "N/A", "N/A"]
-COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
-          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+COLORS = [(0.000, 0.447, 0.741), (0.850, 0.325, 0.098), (0.929, 0.694, 0.125),
+          (0.494, 0.184, 0.556), (0.466, 0.674, 0.188), (0.301, 0.745, 0.933)]
 transform = T.Compose([
         T.Resize(800),
         T.ToTensor(),
@@ -58,7 +58,14 @@ def detect(im, model, transform):
 
 
 def plot_results(pil_img, prob, boxes, img_name):
-    plt.figure(figsize=(40, 30))
+    # pil_img_draw = ImageDraw.Draw(pil_img)
+    # for p, (xmin, ymin, xmax, ymax) in zip(prob, boxes.tolist()):
+    #     pil_img_draw.rectangle([(xmin, ymin), (xmax - xmin, ymax - ymin)], outline="red")
+    #     cl = p.argmax()
+    #     text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+    #     pil_img_draw.text((xmin, ymin), text)
+    # pil_img.save(f"static/input_images/{img_name}")
+    plt.figure(figsize=pil_img.size)
     plt.imshow(pil_img)
     ax = plt.gca()
     for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
@@ -69,7 +76,7 @@ def plot_results(pil_img, prob, boxes, img_name):
         ax.text(xmin, ymin, text, fontsize=15,
                 bbox=dict(facecolor='yellow', alpha=0.5))
     plt.axis('off')
-    plt.savefig(f"static/input_images/{img_name}")
+    plt.savefig(f"static/input_images/{img_name}", bbox_inches='tight')
 
 
 @app.route('/')
